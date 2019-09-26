2aa1580fb5ef3a9400bde94db47502c6da0dba9e
mauricio s <msant323@fiu.edu>
2019-09-26 16:47:06 -0400

    add our first end point

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors())
 app.use(express.static('client'))
 
+//--- Our first end point
+app.get('/rick', (req, res) => {
+	res.status(200);
+	res.send("https://screenrant.com/wp-content/uploads/2017/07/Rick-and-Morty.jpg");
+});
+
 app.listen({ port: port }, () => {
     console.log("Server ready at http://localhost:" + port)
 })
