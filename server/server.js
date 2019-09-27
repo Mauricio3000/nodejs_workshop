b47af0955d10d7a8eccd754cc2149f816b6192f4
mauricio s <msant323@fiu.edu>
2019-09-27 00:48:02 -0400

    setup a post api end point, and create a function to use it in the front end

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const port = 8080;
 const express = require("express")
 const cors = require("cors")
+const bodyParser = require("body-parser")
+
 
 //--- Express setup
 const app = express();
 app.use(cors())
 app.use(express.static('client'))
+
+//--- Needed to handle POST method
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //--- Our first end point
 app.get('/rick', (req, res) => {
@@ -19,6 +25,10 @@ app.get('/rs', (req, res) => {
 	res.send("https://static2.srcdn.com/wordpress/wp-content/uploads/2017/03/Ren-and-Stimpy.jpg");
 });
 
+app.post('/text', (req, res) => {
+	console.log(req.body);
+	res.end("Done.");
+});
 
 app.listen({ port: port }, () => {
     console.log("Server ready at http://localhost:" + port)
